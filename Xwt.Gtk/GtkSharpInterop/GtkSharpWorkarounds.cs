6a3cad059df674e545192486300a308284c9f82b
lytico <lytico@users.sourceforge.net>
2020-04-07 17:14:20 +0200

    GtkSharpWorkarounds: refactor color-handling next DRAFT

--- Xwt.Gtk/GtkSharpInterop/GtkSharpWorkarounds.cs
+++ Xwt.Gtk/GtkSharpInterop/GtkSharpWorkarounds.cs
@@ -9,38 +9,48 @@ namespace Xwt.GtkBackend {
         
         public static Color ToGdkColor(this Gdk.RGBA color)=>new Color((byte)(color.Red * 255), (byte)(color.Green * 255), (byte)(color.Blue * 255));
 
-        static void TestGetting_abi_info(Gtk.Style it, Gtk.StateType state) {
-            unsafe {
-                
-                var test = "black";
-                var raw_ptr1 = it.Handle + (int)Gtk.Style.abi_info.GetFieldOffset(test);
-                var z1 =  Marshal.PtrToStructure(raw_ptr1, typeof(Gdk.Color));  
-                var raw_ptr2 = (IntPtr*)(((byte*)it.Handle) + Gtk.Style.abi_info.GetFieldOffset(test));
-                var z2 =  Gdk.Color.New ((*raw_ptr2));
-                var z3 =   Marshal.PtrToStructure((*raw_ptr2), typeof(Gdk.Color));  ;
-                
-                var bg = "bg";
-                var ptr_bg = it.Handle + (int)Gtk.Style.abi_info.GetFieldOffset(bg);
-                // seems to work:
-                var bgRes =  Marshal.PtrToStructure<Gtk.Style.GtkStyle_bgAlign>(ptr_bg);
-                
-                // crash:
-                if (false) {
-                    var ptr_bg1 = (IntPtr*) (((byte*) it.Handle) + Gtk.Style.abi_info.GetFieldOffset(bg));
-                    var bgRes1 = Marshal.PtrToStructure(*ptr_bg1, typeof(Gtk.Style.GtkStyle_bgAlign));
-                }
-            }
+        static void StyleContextSandbox(Gtk.StyleContext ctx) {
+            
+            Console.WriteLine($"{string.Join(",",ctx.ListClasses())}");
         }
-        public static Gdk.Color Background(this Gtk.Style it, Gtk.StateType state) {
-            var ctx = it.Context;
-            var color = ctx.GetBackgroundColor(state.ToGtk3StateFlags());
-            var col = color.ToGdkColor();
 
-            if (false) {
-                TestGetting_abi_info(it, state);
+        public static Gdk.Color ColorFor(this Gtk.StyleContext ctx, string postfix, Gtk.StateType state) {
+            var prefix = string.Empty;
+            // see: https://developer.gnome.org/gtk3/stable/gtk-migrating-GtkStyleContext-css.html
+            // examples: (see: gtk.css) selected_bg_color insensitive_bg_color base_color theme_text_color insensitive_base_color theme_unfocused_fg_color theme_unfocused_text_color theme_unfocused_bg_color
+
+            switch (state) {
+                case StateType.Normal:
+                    prefix = "theme_unfocused_";
+                    break;
+                case StateType.Active:
+                    break;
+                case StateType.Prelight:
+                    break;
+                case StateType.Selected:
+                    prefix = "selected_";
+                    break;
+                case StateType.Insensitive:
+                    prefix = "insensitive_";
+                    break;
+                case StateType.Inconsistent:
+                    break;
+                case StateType.Focused:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(state), state, null);
+            }
+            if (ctx.LookupColor($"{prefix}{postfix}_color", out var col)) {
+                return col.ToGdkColor();
             }
 
-            return col;
+            ctx.LookupColor("base_color", out col);
+            return col.ToGdkColor();
+        }
+        
+        public static Gdk.Color Background(this Gtk.Style it, Gtk.StateType state) {
+
+            return it.Context.ColorFor("bg", state);
         }
         
         public static Gdk.Color Dark(this Gtk.Style it, Gtk.StateType state)
@@ -52,15 +62,13 @@ namespace Xwt.GtkBackend {
         
         public static Gdk.Color Foreground(this Gtk.Style it, Gtk.StateType state)
         {
-            var ctx = it.Context;
-            var color = ctx.GetColor(state.ToGtk3StateFlags());
-            return color.ToGdkColor();
+            return it.Context.ColorFor("fg", state);
+          
         }
         
         public static Gdk.Color Base(this Gtk.Style it, Gtk.StateType state)
         {
-           // TODO
-           return it.Foreground(state);
+            return it.Context.ColorFor("", state);
         }
 
         public static void ModifyBase(this Gtk.Widget it, Gtk.StateType state, Gdk.Color color) {
