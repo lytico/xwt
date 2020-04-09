c73bb9eb9459a45e4194eb79e0528d30360731e1
lytico <lytico@users.sourceforge.net>
2020-04-09 19:55:34 +0200

    Gtk.ListStoreBackend: handle RowsReordered in InitializeModel

--- Xwt.Gtk/Xwt.GtkBackend/ListStoreBackend.cs
+++ Xwt.Gtk/Xwt.GtkBackend/ListStoreBackend.cs
@@ -33,7 +33,7 @@ using TreeModel = Gtk.ITreeModel;
 
 namespace Xwt.GtkBackend
 {
-	public class ListStoreBackend: TableStoreBackend, IListStoreBackend
+	public partial class ListStoreBackend: TableStoreBackend, IListStoreBackend
 	{
 		public override TreeModel InitializeModel (Type[] columnTypes)
 		{
@@ -50,9 +50,10 @@ namespace Xwt.GtkBackend
 				if (RowChanged != null)
 					RowChanged (this, new ListRowEventArgs (args.Path.Indices[0]));
 			};
-			store.RowsReordered += (o, args) => {
-				if (RowsReordered != null) System.Diagnostics.Debug.WriteLine ($"No support for {nameof (RowsReordered)} events from {nameof (ListStoreBackend)}, sorry.");
-			};
+			InitializeModelFix(columnTypes, store);
+			// store.RowsReordered += (o, args) => {
+			// 	if (RowsReordered != null) System.Diagnostics.Debug.WriteLine ($"No support for {nameof (RowsReordered)} events from {nameof (ListStoreBackend)}, sorry.");
+			// };
 			return store;
 		}
 		
