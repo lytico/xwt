13a2e9c4e15a0e328e465c0301183c9218a7b7b7
lytico <lytico@users.sourceforge.net>
2020-04-09 19:53:10 +0200

    Gtk.FileDialogBackend: add more translations

--- Xwt.Gtk/Xwt.GtkBackend/FileDialogBackend.cs
+++ Xwt.Gtk/Xwt.GtkBackend/FileDialogBackend.cs
@@ -67,17 +67,17 @@ namespace Xwt.GtkBackend
 
 			switch (action) {
 				case Gtk.FileChooserAction.Open:
-					dialog.AddButton (Gtk.Stock.Cancel, Gtk.ResponseType.Cancel);
-					dialog.AddButton (Gtk.Stock.Open, Gtk.ResponseType.Ok);
+					dialog.AddButton (Application.TranslationCatalog.GetString(Gtk.Stock.Cancel), Gtk.ResponseType.Cancel);
+					dialog.AddButton (Application.TranslationCatalog.GetString(Gtk.Stock.Open), Gtk.ResponseType.Ok);
 					break;
 				case Gtk.FileChooserAction.SelectFolder:
 				case Gtk.FileChooserAction.CreateFolder:
-					dialog.AddButton (Gtk.Stock.Cancel, Gtk.ResponseType.Cancel);
+					dialog.AddButton (Application.TranslationCatalog.GetString(Gtk.Stock.Cancel), Gtk.ResponseType.Cancel);
 					dialog.AddButton (Application.TranslationCatalog.GetString("Select"), Gtk.ResponseType.Ok);
 					break;
 				case Gtk.FileChooserAction.Save:
-					dialog.AddButton (Gtk.Stock.Cancel, Gtk.ResponseType.Cancel);
-					dialog.AddButton (Gtk.Stock.Save, Gtk.ResponseType.Ok);
+					dialog.AddButton (Application.TranslationCatalog.GetString(Gtk.Stock.Cancel), Gtk.ResponseType.Cancel);
+					dialog.AddButton (Application.TranslationCatalog.GetString(Gtk.Stock.Save), Gtk.ResponseType.Ok);
 					break;
 				default:
 					break;
